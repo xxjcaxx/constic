c3a550ad13c0b1e381563b422fbaa81ea00b1e3c
xxjcaxx <xxjcaxx@yahoo.es>
2025-07-03 16:04:29 +0200

    borken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   organizationName: 'xxjcaxx', // Usually your GitHub org/user name.
   projectName: 'constic', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
